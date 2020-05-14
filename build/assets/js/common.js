103392de0a40b02792858f56b70c7256069b9f49
Zhycho <avd.qu6@gmail.com>
2020-05-14 19:21:10 +0500

    Доделал страницу публикации и сделал страницу индивидуального продукта

--- build/assets/js/common.js
+++ build/assets/js/common.js
@@ -15,7 +15,38 @@ $(document).ready(function() {
         prevArrow: $('.js--products-from-publication__prev'),
         nextArrow: $('.js--products-from-publication__next'),
         dots: false,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        responsive: [
+            {
+              breakpoint: 1201,
+              settings: {
+                slidesToShow: 3
+              }
+            },
+            {
+              breakpoint: 769,
+              settings: {
+                slidesToShow: 2
+              }
+            },
+            {
+              breakpoint: 577,
+              settings: {
+                slidesToShow: 1
+              }
+            },
+          ]
+    });
+
+    // Слайдер индивидуальных продуктов
+    $('.js--individual-product-slider').slick({
+        infinite: false,
+        slidesToShow: 1,
+        arrows: false,
+        dots: false,
+        slidesToScroll: 1,
+        prevArrow: $('.js--individual-product-slider__prev'),
+        nextArrow: $('.js--individual-product-slider__next'),
     });
 
 
